3d95964c6ed1756a1254cfb86e7170078d3f0672
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-09 09:38:15 -0600

    Usando hook useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const LoginPage = () => {
+  const prueba = []
+
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate('/' , {
+      replace: true
+    })
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr />
+
+      <button
+        onClick={onLogin}
+        className='btn btn-primary'>Login</button>
+    </div>
   )
 }
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace : true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -35,7 +44,9 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>Uriel</span>
 
-                    <button className='nav-item nav-link btn'>Logout</button>
+                    <button
+                        onClick={onLogout} 
+                        className='nav-item nav-link btn'>Logout</button>
                 </ul>
             </div>
         </nav>
