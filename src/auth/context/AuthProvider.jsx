d65e1b1613b914a015b8dc0f07c10574941e30d3
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-10 11:48:39 -0600

    Agregando localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,23 +3,31 @@ import { types } from '../types/types'
 import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
 
-const initialState = {
-  logged: false
+
+
+const init = () => {
+  const user =  JSON.parse(localStorage.getItem('user'))
+
+  return{
+    logged: !!user,
+    user: user
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer( authReducer, initialState)
+  const [ authState, dispatch ] = useReducer( authReducer, {} , init )
 
   const onLogin = ( name = '') => {
 
+    const user = { id: 'ABC', name: name}
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name
-      }
+      payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify( user ))
 
     dispatch( action )
   }
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -53,7 +53,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-primary'>{ user.name }</span>
+                    <span className='nav-item nav-link text-primary'>{ user?.name }</span>
 
                     <button
                         onClick={onLogout} 
