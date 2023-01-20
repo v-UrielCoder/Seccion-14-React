10d29994096278ece1e65920741068a4bbbd55ff
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-20 13:07:24 -0600

    Pruebas en la ruta publica

---
+++ test/router/PublicRoute.test.jsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter, Route, Routes } from "react-router-dom"
+import { AuthContext } from "../../src/auth"
+import { PublicRoute } from "../../src/router/PublicRoute"
+
+describe('pruebas en PublicRoute', () => {
+
+  test('debe de mostrat el children si no esta autenticado', () => {
+
+    const contextValue = {
+      logged: false
+    }
+    render (
+      <AuthContext.Provider value={contextValue} >
+        <PublicRoute>
+          <h1>Ruta publica</h1>
+        </PublicRoute>
+      </AuthContext.Provider>
+    )
+
+    expect( screen.getByText('Ruta publica') ).toBeTruthy();
+  })
+
+  test('debe de navegar si esta autentificado', () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: 'Strider',
+        id: '123'
+      }
+    }
+    render (
+      <AuthContext.Provider value={contextValue} >
+
+        <MemoryRouter initialEntries={['/login']}>
+
+          <Routes>
+            <Route path="marvel"element={<h1>Pagina marvel</h1>}/>
+
+            <Route path="login" element = {
+              <PublicRoute>
+                <h1>Ruta publica</h1>
+              </PublicRoute>
+            }/>
+
+          </Routes>
+        </MemoryRouter>
+
+      </AuthContext.Provider>
+    )
+    
+    expect( screen.getByText('Pagina marvel') ).toBeTruthy()
+  })
+})
